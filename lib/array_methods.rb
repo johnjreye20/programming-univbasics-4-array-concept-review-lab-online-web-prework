3d7d7c750b2bb9ee409a4e47bd2342271e58fd37
John Reyes <john.jreye@gmail.com>
2019-08-24 23:22:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+find_element = ["array", "value_to_find"]
+  find_element.index("array")
 end
 
-def find_max_value(array)
-  # Add your solution here
+def digits = [0..9]
+  puts digits.max
 end
 
-def find_min_value(array)
-  # Add your solution here
+def numbers [10...25]
+  numbers.min
 end
